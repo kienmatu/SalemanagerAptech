a8bc18a8ef94739e4670d9c1144ddac72e22f9ac
kiendt <kienmatu1@gmail.com>
2018-11-29 17:37:11 +0700

    Helloworld .

--- src/telephonerepository/TelephoneRepository.java
+++ src/telephonerepository/TelephoneRepository.java
@@ -16,6 +16,8 @@ public class TelephoneRepository {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+System.out.println("Helloworld");
+
     }
     
 }
