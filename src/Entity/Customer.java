a63228c496b4c346ee7f86560abc76b1b8091f0f
Datthanh123 <datthanhsl00@gmail.com>
2018-12-10 22:54:09 +0700

    ok

--- src/Entity/Customer.java
+++ src/Entity/Customer.java
@@ -11,6 +11,8 @@ import java.util.Date;
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
@@ -40,6 +42,7 @@ public class Customer implements Serializable {
 
     private static final long serialVersionUID = 1L;
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Basic(optional = false)
     @Column(name = "CUSTID")
     private Integer custid;
